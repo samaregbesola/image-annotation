e609ffb3f8958afe844d76145d6cdda404dbe916
Areson <samuelaregbesola@gmail.com>
2023-05-29 14:39:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,102 @@
+import { useRef, useEffect, useState } from "react";
+
+const App = () => {
+  let imageUrl = "https://source.unsplash.com/1980x1020/?beach";
+  const canvasRef = useRef(null);
+  const [canvasWidth, setCanvasWidth] = useState(0);
+  const [canvasHeight, setCanvasHeight] = useState(0);
+  const [drawing, setDrawing] = useState(false);
+  const [brushWidth, setBrushWidth] = useState(5);
+  const [brushColor, setBrushColor] = useState("#000000"); // new state variable for brush color
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    const image = new Image();
+
+    image.onload = () => {
+      setCanvasWidth(window.innerWidth);
+      setCanvasHeight(window.innerHeight * 0.9);
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    };
+
+    image.src = imageUrl;
+  }, [imageUrl]);
+
+  const handleMouseDown = (event) => {
+    setDrawing(true);
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    const { offsetX, offsetY } = event.nativeEvent;
+
+    context.beginPath();
+    context.moveTo(offsetX, offsetY);
+  };
+
+  const handleMouseMove = (event) => {
+    if (!drawing) return;
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    const { offsetX, offsetY } = event.nativeEvent;
+
+    context.lineWidth = brushWidth;
+    context.strokeStyle = brushColor; // set the stroke style based on brushColor state variable
+    context.lineTo(offsetX, offsetY);
+    context.stroke();
+  };
+
+  const handleMouseUp = () => {
+    setDrawing(false);
+  };
+
+  const handleBrushWidthChange = (event) => {
+    setBrushWidth(event.target.value);
+  };
+
+  const handleBrushColorChange = (event) => {
+    setBrushColor(event.target.value);
+  };
+
+  return (
+    <div>
+      <canvas
+        ref={canvasRef}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        width={canvasWidth}
+        height={canvasHeight}
+        style={{ width: "100%", height: "90vh" }}
+      />
+      <div>
+        <label htmlFor="brush-width">Brush Width:</label>
+        <input
+          type="range"
+          id="brush-width"
+          name="brush-width"
+          min="1"
+          max="20"
+          value={brushWidth}
+          onChange={handleBrushWidthChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="brush-color">Brush Color:</label>
+        <select
+          id="brush-color"
+          name="brush-color"
+          value={brushColor}
+          onChange={handleBrushColorChange}
+        >
+          <option value="#000000">Black</option>
+          <option value="#ff0000">Red</option>
+          <option value="#0000ff">Blue</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default App;
